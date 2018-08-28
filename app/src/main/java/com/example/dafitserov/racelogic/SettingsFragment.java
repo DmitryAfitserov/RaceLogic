f14036172dacc2da4ea7fcdf6293671add61352a
dmitryafitserov <dmitryafitserov@gmail.com>
2018-08-28 16:00:04 +0300

    create ListPreference in code(listener)

--- app/src/main/java/com/example/dafitserov/racelogic/SettingsFragment.java
+++ app/src/main/java/com/example/dafitserov/racelogic/SettingsFragment.java
@@ -1,21 +1,33 @@
 package com.example.dafitserov.racelogic;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 
 public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        ListPreference listPreference = (ListPreference)getPreferenceManager().findPreference("but_list_speed_from");
+
+        listPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+                return true;
+            }
+        });
     }
-
-
 
 
 }
