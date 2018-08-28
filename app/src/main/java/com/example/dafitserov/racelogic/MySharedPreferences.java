298cdbd18a2188ee7725e9080bd0e20f229c06fa
dmitryafitserov <dmitryafitserov@gmail.com>
2018-08-28 17:50:58 +0300

    creating logic in choose value speed for ListPreference in code(listener)

---
+++ app/src/main/java/com/example/dafitserov/racelogic/MySharedPreferences.java
@@ -0,0 +1,6 @@
+package com.example.dafitserov.racelogic;
+
+public class MySharedPreferences {
+
+
+}
--- app/src/main/java/com/example/dafitserov/racelogic/SettingsFragment.java
+++ app/src/main/java/com/example/dafitserov/racelogic/SettingsFragment.java
@@ -11,23 +11,65 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class SettingsFragment extends PreferenceFragment {
+
+    int speedFrom;
+    int speedTo;
+
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
 
-        ListPreference listPreference = (ListPreference)getPreferenceManager().findPreference("but_list_speed_from");
+        final ListPreference listPreferenceSpeedFrom =
+                (ListPreference)getPreferenceManager().findPreference("but_list_speed_from");
 
-        listPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+        final ListPreference listPreferenceSpeedTo =
+                (ListPreference)getPreferenceManager().findPreference("but_list_speed_to");
+
+        Log.d("EEE", "cvc   " + listPreferenceSpeedFrom.getValue() + "  df  " +   listPreferenceSpeedTo.getValue());
+        String a = listPreferenceSpeedFrom.getValue();
+        String b = listPreferenceSpeedTo.getValue();
+
+        speedFrom = Integer.parseInt(a);
+        speedTo = Integer.parseInt(b);
+
+
+
+        listPreferenceSpeedFrom.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object o) {
+                int choosedSpeed = Integer.parseInt(listPreferenceSpeedFrom.getValue());
+                    if(choosedSpeed >= speedTo){
+                        Toast.makeText(getActivity(),  R.string.toast_fail_choosed_speed_to, Toast.LENGTH_LONG);
+                        listPreferenceSpeedTo.setValue(String.valueOf(R.string.max_speed_value));
+                        listPreferenceSpeedTo.setSummary(R.string.max_speed);
+                        speedTo = Integer.parseInt(String.valueOf(R.string.max_speed_value));
+                        speedFrom = choosedSpeed;
+                    }
                 return true;
             }
         });
+
+        listPreferenceSpeedTo.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+
+                return true;
+            }
+        });
+
+
     }
 
+    private void setIntSpeed(){
+
+    }
 
 }
